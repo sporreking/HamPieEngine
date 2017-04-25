f300b1c0d411984400b2de5f5c5a5e7096ffb405
Ed <edvard.thornros@gmail.com>
2017-04-25 18:47:39 +0200

    Some peopel would claim the physics actually work now, I doubt it.

--- src/TestState.java
+++ src/TestState.java
@@ -2,6 +2,7 @@ import java.awt.Font;
 
 import org.lwjgl.glfw.GLFW;
 
+import Debug.Debug;
 import sk.audio.Audio;
 import sk.audio.AudioManager;
 import sk.audio.SineAudio;
@@ -93,6 +94,12 @@ public class TestState implements GameState {
 				new Vector2f(-1.5f, -0.5f),
 				new Vector2f(-0.5f, -0.5f),
 				new Vector2f(-0.5f,  0.5f));
+
+		Shape s_shape3 = new Shape(
+				new Vector2f(-1.5f,  0.5f),
+				new Vector2f(-1.5f, -0.5f),
+				new Vector2f(-2.5f, -1.5f),
+				new Vector2f(-2.5f,  1.5f));
 		
 		Transform t = new Transform();
 		t.position.y = 1.0f;
@@ -124,13 +131,14 @@ public class TestState implements GameState {
 		t.position.y = 0.0f;
 		t.position.x = 1.0f;
 		t.scale.x = 2.0f;
+		t.scale.y = 2.0f;
 		t_entity.add(0, t);
 		t_entity.add(0,	new Renderer(Mesh.QUAD));
 		t_entity.add(0, new Body(s_shape, 1.0f, 100.0f, 0.2f));
 		t_entity.get(Body.class).setDynamic(false);
 		t_entity.get(Body.class).addShape(s_shape2);
+		t_entity.get(Body.class).addShape(s_shape3);
 		world.addEntity(t_entity);
-		
 
 		t_root.add(0, "Test3", t_entity);
 		
@@ -208,6 +216,7 @@ public class TestState implements GameState {
 	@Override
 	public void draw() {
 		t_root.draw();
+		Debug.draw();
 	}
 	
 	@Override
