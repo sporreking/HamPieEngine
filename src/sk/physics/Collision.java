df395077c95d30bfcd5c55f7cd6fd3c3d39c2d1d
Eddie-boi <edvard.thornros@gmail.com>
2017-04-27 14:09:08 +0200

    Fixed some shit... Just don't ask.

--- src/sk/physics/Collision.java
+++ src/sk/physics/Collision.java
@@ -1,8 +1,5 @@
 package sk.physics;
 
-
-import com.sun.media.sound.DirectAudioDeviceProvider;
-
 import sk.gfx.Transform;
 import sk.util.vector.Vector2f;
 
@@ -94,8 +91,6 @@ public class Collision {
 				b.getCenter(tb),
 				null);
 		
-		
-		
 		float max;
 		float min;
 		float dotDistance = 0.0f;
@@ -172,8 +167,6 @@ public class Collision {
 			normal.negate();
 		}
 		
-		
-		
 		// Move it back
 		Vector2f reverse;
 		if (dynamicCollision) {
@@ -184,7 +177,7 @@ public class Collision {
 		}
 		b.getTransform().position.add(reverse);
 		
-		// Change the velocity
+		// Check the relative velocity
 		Vector2f relativeVelocity = new Vector2f();
 		Vector2f.sub(a.getNextVelocity(), b.getNextVelocity(), relativeVelocity);
 		
@@ -192,6 +185,8 @@ public class Collision {
 		
 		// Make sure we're not moving away, if we are, just return
 		if (0.0f > normalVelocity) return;
+		// If we only want anti overlap
+		if (a.isOnlyOverlap() || b.isOnlyOverlap()) return;
 		
 		// Bounce
 		float bounce = Math.min(a.getBounce(), b.getBounce());
